c3bbb9ae8c99f95b1882d8d6a039225e41410ff5
marchstuped <50006961+marchstuped@users.noreply.github.com>
2019-08-21 15:47:16 +0700

    Update App.js

--- bmi/App.js
+++ bmi/App.js
@@ -33,7 +33,7 @@ export default class App extends React.Component {
     let height = Number(this.state.height);
     const b = bmi.bmiCalculate(weight , height);
     const crit = bmi.critCalculator(b , this.state.region);
-    Alert.alert(b.toString() , crit);
+    Alert.alert(crit);
   }
 
 
